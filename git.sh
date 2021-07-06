1340666ab107eb460ef230f056e4274f7ffb1986
flavioferretti <flavioferretti@gmail>
2021-07-06 13:21:29 +0200

    message 01

---
+++ git.sh
@@ -0,0 +1,5 @@
+git add *
+git commit -a
+git remote add origin git@github.com:flavioferretti/django-lorenzo.git
+git branch -M main
+git push -u origin main
--- lorenzoProject/magazzino/models.py
+++ lorenzoProject/magazzino/models.py
@@ -1,9 +1,12 @@
 from django.db import models  
+from django import forms
+
 class Magazzino(models.Model):  
 	eid = models.CharField(max_length=20)  
 	ename = models.CharField(max_length=100)  
 	eefirm = models.CharField(max_length=100)  
-	econtact = models.CharField(max_length=250)  
+	edescription = models.TextField( blank=True )  
 	class Meta:  
 		db_table = "magazzino"  
 
+ 
